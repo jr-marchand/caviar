1cb15276a16f5160bdbebd250eed907c20c42f6f
Jean Remy Marchand <jean-remy.marchand@novartis.com>
2020-01-13 10:09:46 +0100

    cleaning the cavitome vs caviar namings

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ with open("README.MD", "r") as fh:
 	long_description = fh.read()
 
 setup(
-	name="cavitome_gui",
+	name="caviar_gui",
 	version="1.0",
 	entry_points = {
-        'console_scripts': [ 'cavitome_gui=cavitome_gui.gui:main']
+        'console_scripts': [ 'caviar_gui=caviar_gui.gui:main']
         },
     author="Jean-Remy Marchand",
 	author_email="jeanremy.marchand@gmail.com",
 	description="A GUI for the automatic detection of protein cavities and their segmentation in subcavities",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	url="https://github.com/jr-marchand/cavitome",
+	url="https://github.com/jr-marchand/CAVIAR-GUI",
 
 	# When your source code is in a subdirectory under the project root, e.g.
-	# `cavitome_gui/`, it is necessary to specify the `package_dir` argument.
-	package_dir={'':'src'},	# tell distutils packages are under cavitome_gui
+	# `caviar_gui/`, it is necessary to specify the `package_dir` argument.
+	package_dir={'':'src'},	# tell distutils packages are under caviar_gui
 
 	# You can just specify package directories manually here if your project is
 	# simple. Or you can use find_packages().
@@ -28,10 +28,10 @@ setup(
 	# called `my_module.py` to exist:
 	#
 	#	py_modules=["my_module"],
-	packages=find_namespace_packages(where='src'), # include all packages under cavitome_gui
+	packages=find_namespace_packages(where='src'), # include all packages under caviar_gui
 
-	#package_data={'':['cavitome_gui/*ui', "cavitome_gui/prody_parser/utilities/datafiles/mod_res_map.dat",
-	#"cavitome_gui/misc_tools/tabu_lists/tabu*", 'cavitome_gui/cavity_identification/vdw_size_atoms.dat']},
+	#package_data={'':['caviar_gui/*ui', "caviar_gui/prody_parser/utilities/datafiles/mod_res_map.dat",
+	#"caviar_gui/misc_tools/tabu_lists/tabu*", 'caviar_gui/cavity_identification/vdw_size_atoms.dat']},
 	include_package_data=True,
 
 	#install_requires=['numpy>=1.17.3',
